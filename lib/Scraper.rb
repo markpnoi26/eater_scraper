1594ff36395ba8cd1f396d9b843c75b816f41011
Mark Daniel Delgado <markpnoi26@gmail.com>
2019-10-01 21:16:33 +0000

    updated scraper.rb

--- lib/Scraper.rb
+++ lib/Scraper.rb
@@ -1,33 +1,42 @@
 require "nokogiri"
 require "open-uri"
 
+# url = "https://www.eater.com/cities-directory"
+# doc = Nokogiri::HTML(open(url))
+
+# cities = doc.css("div.c-directory__short-body")
+# cities.search("h2").text #city names array
+# cities.search("h2").css("a").attribute("href").value #city urls
+
 class Scraper
+  attr_accessor :array
   
-  def self.cities_scraper(url)
-    #returns an array of city hashes. 
-    array = [
-      {
-        :name => "New York",
-        :url => "url.newyork.eater"
-      },
-      {
-        :name => "Seattle"
-        :url => "url.seattle.eater"
+  def self.cities_scraper
+    
+    @array = []
+    doc = Nokogiri::HTML(open("https://www.eater.com/cities-directory"))
+    cities = doc.css("div.c-directory__short-body")
+    cities.each do |city|
+      city_hash = {
+        :name => city.search("h2").text,
+        :url => city.search("h2").css("a").attribute("href").value
       }
-      ]
+      @array << city_hash
+    end
+    @array
   end
   
-  def self.latest_scraper(url)
-    #returns a hash of each latest news for the given city
-    article_array = [{
-      :title => "Best food in Seattle",
-      :authors => ["Joe Dane", "Roe Wade"],
-      :url => "url.bestfoodinseattle.eater"
-    }, {
-      :title => "Best food in Atlanta",
-      :authors => ["One Roe"]
-      :url => "url.bestfoodinatlanta.eater"
-    }]
-  end
+  # def self.latest_scraper(url)
+  #   #returns a hash of each latest news for the given city
+  #   article_array = [{
+  #     :title => "Best food in Seattle",
+  #     :authors => ["Joe Dane", "Roe Quade"],
+  #     :url => "url.bestfoodinseattle.eater"
+  #   }, {
+  #     :title => "Best food in Atlanta",
+  #     :authors => ["One Roe"]
+  #     :url => "url.bestfoodinatlanta.eater"
+  #   }]
+  # end
   
 end
