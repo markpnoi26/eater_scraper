afaa3311dd7f9a4be95145dcc81158f87376e81e
Mark Daniel Delgado <markpnoi26@gmail.com>
2019-10-01 20:20:33 +0000

    Scraper.rb updated

--- lib/Scraper.rb
+++ lib/Scraper.rb
@@ -1,7 +1,33 @@
+require "nokogiri"
+require "open-uri"
+
 class Scraper
   
-  def hello
-    
-    puts "hello"
+  def self.cities_scraper(url)
+    #returns an array of city hashes. 
+    array = [
+      {
+        :name => "New York",
+        :url => "url.newyork.eater"
+      },
+      {
+        :name => "Seattle"
+        :url => "url.seattle.eater"
+      }
+      ]
   end
+  
+  def self.latest_scraper(url)
+    #returns a hash of each latest news for the given city
+    article_array = [{
+      :title => "Best food in Seattle",
+      :authors => ["Joe Dane", "Roe Wade"],
+      :url => "url.bestfoodinseattle.eater"
+    }, {
+      :title => "Best food in Atlanta",
+      :authors => ["One Roe"]
+      :url => "url.bestfoodinatlanta.eater"
+    }]
+  end
+  
 end
