8f798dd7337d491f08f8c85e90de0751a393d161
Mark Daniel Delgado <markpnoi26@gmail.com>
2019-10-13 10:18:46 -0700

    deployment

--- lib/eater_scraper.rb
+++ lib/eater_scraper.rb
@@ -6,8 +6,3 @@ require_relative "Scraper.rb"
 require_relative "Article.rb"
 require_relative "City.rb"
 require_relative "CLI_Controller.rb"
-
-# module EaterScraper
-#   class Error < StandardError; end
-#   # Your code goes here...
-# end
